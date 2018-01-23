bf73fb3e51f848e43ed55b84b05a3c4455f8ddec
zcong1993 <1005997919@qq.com>
2018-01-23 23:57:40 +0800

    update index.d.ts

--- index.d.ts
+++ index.d.ts
@@ -2,15 +2,15 @@ import { Chalk } from 'chalk'
 
 declare namespace ColorUtils {
   const chalk: Chalk
-  function success(...strs:string[]): void
-  function info(...strs:string[]): void
-  function warning(...strs:string[]): void
-  function error(...strs:string[]): void
-  function bgSuccess(...strs:string[]): void
-  function bgInfo(...strs:string[]): void
-  function bgWarning(...strs:string[]): void
-  function bgError(...strs:string[]): void
-  function logPad(fn: (...strs:string[]) => void, ...strs:string[]): void
+  function success(...strs:string[]): string
+  function info(...strs:string[]): string
+  function warning(...strs:string[]): string
+  function error(...strs:string[]): string
+  function bgSuccess(...strs:string[]): string
+  function bgInfo(...strs:string[]): string
+  function bgWarning(...strs:string[]): string
+  function bgError(...strs:string[]): string
+  function logPad(fn: (...strs:string[]) => string, ...strs:string[]): void
 }
 
 export = ColorUtils
