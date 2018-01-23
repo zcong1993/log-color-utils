beb7e026493f94f81474724719ba404d9046ef37
zcong1993 <1005997919@qq.com>
2018-01-23 18:48:02 +0800

    add functions

--- index.d.ts
+++ index.d.ts
@@ -1,12 +1,16 @@
 import { Chalk } from 'chalk'
 
 declare namespace ColorUtils {
-  declare const chalk: Chalk
-  declare function success(...strs:string): void
-  declare function info(...strs:string): void
-  declare function warning(...strs:string): void
-  declare function error(...strs:string): void
-  declare function logPad(fn: (...strs:string) => void, ...strs:string): void
+  const chalk: Chalk
+  function success(...strs:string[]): void
+  function info(...strs:string[]): void
+  function warning(...strs:string[]): void
+  function error(...strs:string[]): void
+  function bgSuccess(...strs:string[]): void
+  function bgInfo(...strs:string[]): void
+  function bgWarning(...strs:string[]): void
+  function bgError(...strs:string[]): void
+  function logPad(fn: (...strs:string[]) => void, ...strs:string[]): void
 }
 
 export = ColorUtils
--- index.js
+++ index.js
@@ -2,20 +2,40 @@ const chalk = require('chalk')
 
 const success = (...strs) => {
   const str = strs.join(' ')
-  return `${chalk.reset.inverse.bold.green(' SUCESS ')} ${str}`
+  return `${chalk.green('success')} ${str}`
 }
 
 const info = (...strs) => {
   const str = strs.join(' ')
-  return `${chalk.reset.inverse.bold.blue(' INFO ')} ${str}`
+  return `${chalk.blue('info')} ${str}`
 }
 
 const warning = (...strs) => {
   const str = strs.join(' ')
-  return `${chalk.reset.inverse.bold.yellow(' WARN ')} ${str}`
+  return `${chalk.yellow('warn')} ${str}`
 }
 
 const error = (...strs) => {
+  const str = strs.join(' ')
+  return `${chalk.red('error')} ${str}`
+}
+
+const bgSuccess = (...strs) => {
+  const str = strs.join(' ')
+  return `${chalk.reset.inverse.bold.green(' SUCESS ')} ${str}`
+}
+
+const bgInfo = (...strs) => {
+  const str = strs.join(' ')
+  return `${chalk.reset.inverse.bold.blue(' INFO ')} ${str}`
+}
+
+const bgWarning = (...strs) => {
+  const str = strs.join(' ')
+  return `${chalk.reset.inverse.bold.yellow(' WARN ')} ${str}`
+}
+
+const bgError = (...strs) => {
   const str = strs.join(' ')
   return `${chalk.reset.inverse.bold.red(' ERROR ')} ${str}`
 }
@@ -30,5 +50,9 @@ module.exports = {
   info,
   warning,
   error,
-  logPad
+  logPad,
+  bgSuccess,
+  bgInfo,
+  bgWarning,
+  bgError
 }
